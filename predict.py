9305db86ed3789e187548d52197e7c7c857e3701
Jon <Jon@singlestonevdt.myfiosgateway.com>
2020-09-22 23:57:02 -0400

    Updating train and predict files

--- predict.py
+++ predict.py
@@ -12,9 +12,9 @@ def main():
     parser.add_argument('--top_k', default=5)
     parser.add_argument('--category_names', default='cat_to_name.json')
     parser.add_argument('--gpu', default=True)
-    parser.add_argument('--show_plots', default=True)
-    args = parser.parse_args()
+    parser.add_argument('--show_plots', default=False)
     global args
+    args = parser.parse_args()
     model = load_model()
 
     if not args.show_plots:
@@ -43,15 +43,26 @@ def process_image(image_path):
 
 def predict(image_path, model, topk=5):
     # Transform
-    cat_to_name = json.loads(open(args.cat_to_name, 'r').read())
+    cat_to_name = json.loads(open(args.category_names, 'r').read())
+    actual_category = cat_to_name[image_path.split('/')[2]]
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     inputs = process_image(image_path).unsqueeze(0)
     model.eval()
     model, inputs = model.to(device), inputs.to(device)
     prob = nnf.softmax(model(inputs), dim=1)
     top_p, top_class = prob.topk(topk, dim = 1)
-    return pd.DataFrame({'probability': top_p.cpu().detach().numpy()[0],
+    pred = pd.DataFrame({'probability': top_p.cpu().detach().numpy()[0],
                          'category': top_class.cpu().detach().numpy()[0]})
+    ppred = pred.sort_values(by='probability', ascending=False)[
+        ['category', 'probability']
+    ]
+    pred['name'] = pred.category.apply(
+        lambda x: cat_to_name[str(int(x))]
+    )
+    pred['category'] = pred['category'].astype(int)
+    print('\n','*'*30, f'\nACTUAL FLOWER NAME: {actual_category}\n', '*'*30)
+    print(pred)
+    return pred
 
 def imshow(image, ax=None, title=None):
     """Imshow for Tensor."""
@@ -74,7 +85,7 @@ def imshow(image, ax=None, title=None):
     return ax
 
 def predict_and_plot(model, img_path):
-    cat_to_name = json.loads(open(args.cat_to_name, 'r').read())
+    cat_to_name = json.loads(open(args.category_names, 'r').read())
     imshow(process_image(img_path))
     pred = predict(img_path, model)
     pred = pred.sort_values(by='probability', ascending=False)[
